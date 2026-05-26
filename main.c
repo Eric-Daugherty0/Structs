79bbfff8923293cee040560a04dc2e5a982c593c
Eric Daugherty <118223484+Eric-Daugherty0@users.noreply.github.com>
2026-05-26 15:43:28 -0400

    Implement car model struct and example in main.c
    Added a struct definition for car models with example usage.

--- main.c
+++ main.c
@@ -1,16 +1,40 @@
+// Understanding structs in STM32
+#include<stdio.h>
 #include<stdint.h>
+
+struct carModel{
+
+	uint32_t carNumber;
+	uint32_t carPrice;
+	uint32_t carMaxSpeed;
+	float carWeight;
+
+};
 
 int main(void)
 {
-	// Using volatile to tell compiler not to optimize these variables
-	// this helps stop the compiler from changing the variables during runtime
-	// producing unexpected changes.
-	uint8_t volatile data1;
-	uint8_t volatile data2;
+	// The first way which order matters.
+	struct carModel carBMW = {2021,15000,220,1330};
+	struct carModel carFord = {4021,35000,160,1900.6};
+	// The second way in which order does not matter.
+	struct carModel carHonda = {.carWeight = 90.89, .carPrice = 15000};
 
-	data1 = 50;
+	// Printing and accessing the members using the dot operator.
+	printf("Details of car BMW is as follows\n");
+	printf("carNumber = %u\n", carBMW.carNumber);
+	printf("carNumber = %u\n", carBMW.carPrice);
+	printf("carNumber = %u\n", carBMW.carMaxSpeed);
+	printf("carNumber = %u\n", carBMW.carWeight);
 
-	data2 = data1;
-	for(;;);
+	// Changing the value
+	carFord.carNumber = 1000;
+
+	printf("Details of car Ford is as follows\n");
+	printf("carNumber = %u\n", carFord.carNumber);
+	printf("carNumber = %u\n", carFord.carPrice);
+	printf("carNumber = %u\n", carFord.carMaxSpeed);
+	printf("carNumber = %u\n", carFord.carWeight);
+
+	return 0;
 
 }
